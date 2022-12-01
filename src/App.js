1da23be2e0184cb4d995346c2f09eccae329aadc
Karan <kkaranoraon@gmail.com>
2022-12-01 20:45:40 +0530

    1-2-2022

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = ()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#343538'
+      document.body.style.color='white'
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='dark'
+    }
+  }
+
+
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About Us" />
+      <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode} />
       <div className="container">
         <TextForm />
       </div>
 
 
-      <footer className="bg-light text-center text-lg-start my-5">
+      <footer className="bg-light text-center text-lg-start my-5 mb-0">
 
         <div className="text-center p-3" >
           © 2022 Copyright:
